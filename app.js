781d38bf7d04d56013c0d5514531e2fd3dca5312
matrixenchant <79454506+matrixenchant@users.noreply.github.com>
2022-01-20 19:43:29 +0600

    new init

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/short.routes'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/'. express.static(path.join(__dirname, 'client', 'build')))
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
